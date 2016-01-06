030f09de506b25de8072fb62e527f9c7cf72a492
avinair <asn2129@columbia.edu>
2016-01-06 11:15:45 -0500

    Modified send-scene button to send only the selected scene

---
+++ include/ros_graspit_interface.h
@@ -0,0 +1,107 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#ifndef _ROS_GRASPIT_INTERFACE_H_
+#define _ROS_GRASPIT_INTERFACE_H_
+
+#include <map>
+#include <QObject>
+#include <QtGui>
+
+//GraspIt! includes
+#include <include/plugin.h>
+
+class GraspitDBModel;
+class Pr2Gripper2010;
+class Body;
+class transf;
+class GraspableBody;
+
+#include <ros/ros.h>
+
+#include <manipulation_msgs/Grasp.h>
+#include <manipulation_msgs/GraspPlanning.h>
+
+#include "include/planning_scene_msg_builder.h"
+#include "grasp_msg_publisher.h"
+
+namespace graspit_ros_planning
+{
+
+//! Main class, combining a ROS node with a GraspIt! interface
+/*! Note that this class inherits from GraspIt's Plugin class and implements the necessary functions to serve
+  as a GraspIt plugin. See include/plugin.h in the GraspIt code for the base class.
+
+  Provides a number of ROS services that directly operate on the GraspIt world, such as loading objects or
+  obstacles, simulating 3D scans of objects, etc.
+
+  In particular, note that this class uses the mainLoop() function to perform the ROS event management calls.
+*/
+class RosGraspitMoveitInterface : public QObject, public Plugin
+{
+
+    Q_OBJECT
+
+private:
+  //! Node handle in the root namespace
+  ros::NodeHandle *root_nh_;
+
+  //! Node handle in the private namespace
+  ros::NodeHandle *priv_nh_;
+
+  PlanningSceneMsgBuilder *mPlanningSceneBuilder;
+  GraspMsgPublisher *mPickupActionGoalBuilder;
+  QComboBox *cb;
+
+public:
+  //! Inits ROS, but (for now) without passing any arguments
+  RosGraspitMoveitInterface();
+  //! Deletes the node handle and the db manager
+  ~RosGraspitMoveitInterface();
+  //! Creates the node handles, advertises services, connects to the database
+  virtual int init(int argc, char **argv);
+  //! Simply calls ros::spinOnce() to process the ROS event loop
+  virtual int mainLoop();
+
+  public Q_SLOTS:
+
+      void onSendPlanningSceneButtonPressed();
+      void onExecuteGraspButtonPressed();
+
+};
+
+
+}
+
+#endif
---
+++ src/ros_graspit_interface.cpp
@@ -0,0 +1,176 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include "include/ros_graspit_interface.h"
+
+#include <boost/foreach.hpp>
+#include <cmath>
+#include <Inventor/actions/SoGetBoundingBoxAction.h>
+
+#include <src/DBase/DBPlanner/ros_database_manager.h>
+#include <src/DBase/graspit_db_model.h>
+#include <src/Collision/collisionStructures.h>
+
+#include <include/mytools.h>
+#include <include/world.h>
+#include <include/body.h>
+#include <include/graspitGUI.h>
+#include <include/ivmgr.h>
+#include <include/scanSimulator.h>
+#include <include/pr2Gripper.h>
+
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <include/EGPlanner/egPlanner.h>
+#include <include/EGPlanner/simAnnPlanner.h>
+
+#include <include/grasp.h>
+#include <include/triangle.h>
+#include <geometry_msgs/Point.h>
+
+#include <thread>
+
+
+namespace graspit_ros_planning
+{
+
+
+RosGraspitMoveitInterface::RosGraspitMoveitInterface() :
+    root_nh_(NULL),
+    priv_nh_(NULL)
+{
+}
+
+RosGraspitMoveitInterface::~RosGraspitMoveitInterface()
+{
+  ROS_INFO("ROS GraspIt node stopping");
+  ros::shutdown();
+  delete root_nh_;
+  delete priv_nh_;
+}
+
+//------------------------- Main class  -------------------------------
+
+int RosGraspitMoveitInterface::init(int argc, char **argv)
+{
+  std::cout << "ThreadId" <<std::this_thread::get_id() << std::endl;
+  //copy the arguments somewhere else so we can pass them to ROS
+  int ros_argc = argc;
+  char** ros_argv = new char*[argc];
+  for (int i = 0; i < argc; i++)
+  {
+    ros_argv[i] = new char[strlen(argv[i])];
+    strcpy(ros_argv[i], argv[i]);
+  }
+  //see if a node name was requested
+  std::string node_name("ros_graspit_interface");
+  for (int i = 0; i < argc - 1; i++)
+  {
+    //std::cerr << argv[i] << "\n";
+    if (!strcmp(argv[i], "_name"))
+    {
+      node_name = argv[i + 1];
+    }
+  }
+  //init ros
+  ros::init(ros_argc, ros_argv, node_name.c_str());
+
+  //init node handles
+  root_nh_ = new ros::NodeHandle("");
+  priv_nh_ = new ros::NodeHandle("~");
+
+  mPlanningSceneBuilder = new PlanningSceneMsgBuilder();
+  mPickupActionGoalBuilder = new GraspMsgPublisher(root_nh_);
+
+  ROS_INFO("Using node name %s", node_name.c_str());
+  for (int i = 0; i < argc; i++)
+  {
+    delete ros_argv[i];
+  }
+  delete ros_argv;
+
+  ROS_INFO("MAKING SHAPE COMPLETION UI");
+
+  QDialogButtonBox *controlBox = new QDialogButtonBox(Qt::Vertical);
+
+  QPushButton * sendPlanningSceneButton = new QPushButton("send Scene", controlBox);
+  sendPlanningSceneButton->setDefault(true);
+  sendPlanningSceneButton->move(0,0);
+
+  cb = new QComboBox(controlBox);
+  cb->move(0,30);
+
+  QPushButton * executeGraspButton = new QPushButton("execute Grasp", controlBox );
+  executeGraspButton->setDefault(true);
+  executeGraspButton->move(0,60);
+
+  controlBox->resize(QSize(200,100));
+  controlBox->show();
+
+  QObject::connect(sendPlanningSceneButton, SIGNAL(clicked()), this, SLOT(onSendPlanningSceneButtonPressed()));
+  QObject::connect(executeGraspButton, SIGNAL(clicked()), this, SLOT(onExecuteGraspButtonPressed()));
+
+  QObject::connect(cb, SIGNAL(activated()), this, SLOT(fillComboBox()));
+
+  ROS_INFO("ROS GraspIt node ready");
+  return 0;
+}
+
+int RosGraspitMoveitInterface::mainLoop()
+{
+  ros::spinOnce();
+  return 0;
+}
+
+void RosGraspitMoveitInterface::onSendPlanningSceneButtonPressed()
+{
+
+    mPlanningSceneBuilder->uploadPlanningSceneToMoveit();
+
+    cb->clear();
+    for(int i = 0; i < graspItGUI->getMainWorld()->getNumGB(); i++)
+    {
+        cb->addItem(graspItGUI->getMainWorld()->getGB(i)->getName());
+    }
+}
+
+void RosGraspitMoveitInterface::onExecuteGraspButtonPressed()
+{
+    int gb_index = cb->currentIndex();
+    mPickupActionGoalBuilder->sendPickupRequest(gb_index);
+}
+
+
+
+}

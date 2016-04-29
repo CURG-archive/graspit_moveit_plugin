d291cee6302c92cb1018bbefda47668d91de2086
Robert Ying <rbtying@aeturnalus.com>
2016-04-29 13:22:21 -0400

    Changed name of library and fixed some CMakeLists things

--- scripts/launch_graspit.sh
+++ scripts/launch_graspit.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-export GRASPIT=$(rospack find graspit)/graspit_source
-export GRASPIT_PLUGIN_DIR=$(dirname $(catkin_find libros_graspit_interface.so))
 
-rosrun graspit graspit -p libros_graspit_interface
+if env | grep -q ^GRASPIT=
+then
+    echo "Using GRASPIT=" $GRASPIT
+else
+    export GRASPIT=$(rospack find graspit)/graspit_source
+fi
+
+export GRASPIT_PLUGIN_DIR=$(dirname $(catkin_find libros_graspit_moveit_interface.so))
+
+rosrun graspit graspit -p libros_graspit_moveit_interface
